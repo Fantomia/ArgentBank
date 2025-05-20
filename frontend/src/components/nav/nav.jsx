7b42c493dfadd6ca98173b2ad4059789692fbd9e
Fantomia <jonathan.andri@gmail.com>
2025-05-20 19:55:31 +0200

    Suppression des erreurs dans la console

--- frontend/src/components/nav/nav.jsx
+++ frontend/src/components/nav/nav.jsx
@@ -31,14 +31,14 @@ const Nav = () => {
             <div className="nav-login">
               <Link to='/profile' className="main-nav-item">
                 {user ? user.userName : ""}
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22" fill="currentColor" class="nav-svg">
-                  <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clip-rule="evenodd" />
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22" fill="currentColor" className="nav-svg">
+                  <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clipRule="evenodd" />
                 </svg>
 
               </Link>
               <Link to="/login" className="main-nav-item" onClick={handleLogout}>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 22 22" stroke-width="2" stroke="currentColor" className="nav-svg">
-                <path stroke-linecap="" stroke-linejoin="" d="M5.636 5.636a9 9 0 1 0 12.728 0M12 3v9" />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 22 22" strokeWidth="2" stroke="currentColor" className="nav-svg">
+                <path strokeLinecap="" strokeLinejoin="" d="M5.636 5.636a9 9 0 1 0 12.728 0M12 3v9" />
               </svg>
 
               </Link>  
