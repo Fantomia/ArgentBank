2aecc836caa2662837cce8af7d583c1a2f08b69b
Fantomia <jonathan.andri@gmail.com>
2026-08-11 17:30:21 +0200

    fix: mise à jour de la connexion mongoose pour Render

--- Backend/server/database/connection.js
+++ Backend/server/database/connection.js
@@ -4,7 +4,10 @@ const databaseUrl =
 
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
+    await mongoose.connect(databaseUrl, {
+      ssl: true,
+      tlsAllowInvalidCertificates: false,
+    })
     console.log('Database successfully connected')
   } catch (error) {
     console.error(`Database Connectivity Error: ${error}`)
